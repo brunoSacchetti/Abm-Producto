363453d5da9397a2d65a69ed2ed4e0c9e8d8a616
brunoSacchetti <sacchettibruno03@gmail.com>
2024-05-23 20:21:59 -0300

    filtro categ screen

--- src/components/ui/tables/TableGeneric/TableGeneric.tsx
+++ src/components/ui/tables/TableGeneric/TableGeneric.tsx
@@ -41,10 +41,11 @@ export const TableGeneric = <T extends { id: any }>({
 }: ITableProps<T>) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  //SEARCH
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredRows, setFilteredRows] = useState<any[]>([]);
   const [categoria, setCategoria] = useState<ICategoria[]>([]);
+  const [selectedCategoriaId, setSelectedCategoriaId] = useState<number>(-1); // -1 para indicar que no hay categoría seleccionada
+  const [productosManufacturados, setProductosManufacturados] = useState<IProductoManufacturado[]>([]);
 
   const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
@@ -71,111 +72,119 @@ export const TableGeneric = <T extends { id: any }>({
     setPage(0);
   }, [searchTerm, dataTable]);
 
-  // #region Categoria Service
   const categoriaService = new CategoriaService(API_URL + "/categoria");
 
   useEffect(() => {
-    categoriaService.getAll().then((data) => {
-      setCategoria(data);
-    });
+    getCategorias();
   }, []);
-
 
   const getCategorias = async () => {
     try {
       const data = await categoriaService.getAll();
       setCategoria(data);
     } catch (error) {
-      console.error("Error al obtener unidades de medida:", error);
+      console.error("Error al obtener categorías:", error);
     }
   };
-  const [selectedCategoriaId, setSelectedCategoriaId] = useState<number>(1);
 
-  const handleChangeCateogiras = async (
+  const handleChangeCategorias = async (
     e: SelectChangeEvent<number>
   ) => {
     const categoriaId = e.target.value as number;
     setSelectedCategoriaId(categoriaId);
-
+    if (categoriaId !== -1) { // Si se selecciona una categoría
+      try {
+        const categoriaSeleccionada = await categoriaService.getById(categoriaId); // Obtener la categoría seleccionada
+        setProductosManufacturados(categoriaSeleccionada?.articulosManufacturados); // Establecer los productos manufacturados asociados a esa categoría
+      } catch (error) {
+        console.error("Error al obtener la categoría:", error);
+      }
+    } else {
+      setProductosManufacturados([]); // Si no hay categoría seleccionada, limpiar la lista de productos manufacturados
+    }
   };
+
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div style={{ display: "flex", marginBottom: 20 }}>
-        <TextField
-          label="Buscar producto"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          style={{ marginRight: 20 }}
-        />
-        <label>Filtrar por categoria</label>
-        <Select
-          label="Categorias"
-          value={selectedCategoriaId ?? ""}
-          onChange={handleChangeCateogiras}
-          variant="filled"
-        >
-          {categoria.map((cat) => (
-            <MenuItem key={cat.id} value={cat.id}>
-              {cat.denominacion}
-            </MenuItem>
-          ))}
-        </Select>
+    <>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ display: "flex", marginBottom: 20 }}>
+          <TextField
+            label="Buscar producto"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            style={{ marginRight: 20 }}
+          />
+          {/* <label>Filtrar por categoría</label> */}
+          <Select
+            label="Categorías"
+            value={selectedCategoriaId ?? ""}
+            onChange={handleChangeCategorias}
+            variant="filled"
+          >
+            <MenuItem value={-1}>Todas las Categorias</MenuItem> {/* Opción para mostrar todas las categorías */}
+            {categoria.map((cat) => (
+              <MenuItem key={cat.id} value={cat.id}>
+                {cat.denominacion}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+        <Paper sx={{ width: "90%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: "80vh" }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column, i: number) => (
+                    <TableCell key={i} align={"center"}>
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(selectedCategoriaId === -1 ? filteredRows : productosManufacturados)
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index: number) => (
+                    <TableRow role="checkbox" tabIndex={-1} key={index}>
+                      {columns.map((column, i: number) => (
+                        <TableCell key={i} align={"center"}>
+                          {column.render ? (
+                            column.render(row)
+                          ) : column.label === "Acciones" ? (
+                            <ButtonsTable
+                              el={row}
+                              handleDelete={handleDelete}
+                              setOpenModal={setOpenModal}
+                              handleCancelOrRegister={handleCancelOrRegister}
+                            />
+                          ) : (
+                            row[column.key]
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={(selectedCategoriaId === -1 ? filteredRows : productosManufacturados).length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
       </div>
-      <Paper sx={{ width: "90%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: "80vh" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column, i: number) => (
-                  <TableCell key={i} align={"center"}>
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredRows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index: number) => (
-                  <TableRow role="checkbox" tabIndex={-1} key={index}>
-                    {columns.map((column, i: number) => (
-                      <TableCell key={i} align={"center"}>
-                        {column.render ? (
-                          column.render(row)
-                        ) : column.label === "Acciones" ? (
-                          <ButtonsTable
-                            el={row}
-                            handleDelete={handleDelete}
-                            setOpenModal={setOpenModal}
-                            handleCancelOrRegister={handleCancelOrRegister}
-                          />
-                        ) : (
-                          row[column.key]
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={filteredRows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </div>
+    </>
   );
 };
